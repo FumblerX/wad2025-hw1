42408ec620c210da8e651155f5aced961ed6589e
FumblerX <114774727+FumblerX@users.noreply.github.com>
2025-02-22 00:13:49 +0300

    Update back.py
    Change render_template path

--- back.py
+++ back.py
@@ -5,11 +5,11 @@ app = Flask(__name__, static_url_path='/static', static_folder='static', templat
 
 @app.route('/')
 def root():
-    return render_template("12.html", code=302)
+    return render_template("redirection_page.html", code=302)
 
 @app.route("/profile")
 def profile():
-    return render_template("123.html", random_number=random.random())
+    return render_template("profile.html", random_number=random.random())
 
 
 if __name__ == "__main__":
